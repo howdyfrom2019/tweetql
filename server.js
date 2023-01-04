845e65f34b55bce265363207045e909b5983bf6f
howdyfrom2019 <tjd1emd@naver.com>
2023-01-04 14:50:24 +0900

    feat: query-mutation set

--- server.js
+++ server.js
@@ -1,13 +1,84 @@
-import { ApolloServer, gql } from "apollo-server";
+import {ApolloServer, gql} from "apollo-server";
+
+let tweets = [
+  {
+    id: "1",
+    text: "hello",
+  },
+  {
+    id: "2",
+    text: "hi",
+  },
+  {
+    id: "3",
+    text: "안녕",
+  }
+];
+
+let users = [
+  {
+    id: "1",
+    username: "성주"
+  }
+];
 
 const typeDefs = gql`
-  type Query {
-    text: String
-    hello: String
-  }
+    type User {
+        id: ID!
+        username: String!
+        nickname: String!
+    }
+    type Tweet {
+        id: ID!
+        text: String!
+        author: User
+    }
+    type Query {
+        allUsers: [User!]!
+        allTweets: [Tweet!]!
+        tweet(id: ID!): Tweet
+        ping: String!
+    }
+    type Mutation {
+        postTweet(text: String!, userId: ID!): Tweet!
+        deleteTweet(id: ID!): Boolean!
+    }
 `;
-const server= new ApolloServer({typeDefs})
 
-server.listen().then(({ url }) => {
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, {id}) {
+      return tweets.find(tweet => tweet.id === id);
+    },
+    allUsers() {
+      return users;
+    }
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = { id : String(tweets.length + 1) , text: String(text) };
+      tweets.push(newTweet);
+
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find(tweet => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter(tweet => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    nickname() {
+      return "jake";
+    }
+  }
+}
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({url}) => {
   console.log(url);
 })
