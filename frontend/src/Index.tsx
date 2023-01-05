e577bd95e3cadfeea7f6f5ae51d22979ceec4717
howdyfrom2019 <tjd1emd@naver.com>
2023-01-05 17:20:54 +0900

    chore: initial project without CRA

---
+++ frontend/src/Index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from 'react-dom/client';
+
+ReactDom.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <div>hihi guys! I'm react</div>
+  </React.StrictMode>
+);
---
+++ frontend/webpack.config.ts
@@ -0,0 +1,90 @@
+import path from 'path';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import webpack, { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+const config: Configuration = {
+  name: 'MusicQL',
+  mode: isDev ? 'development' : 'production',
+  devtool: isDev ? 'hidden-source-map' : 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@/*': path.resolve(__dirname, 'src'),
+    },
+  },
+  entry: {
+    app: './src/Index',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: { browsers: ['IE 10'] },
+                debug: isDev,
+              },
+            ],
+            '@babel/preset-react',
+            '@babel/preset-typescript',
+          ],
+          exclude: path.join(__dirname, 'node_modules'),
+        },
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html'),
+      minify: true,
+    }),
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDev ? 'devlopment' : 'production' }),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist/',
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    devMiddleware: { publicPath: '/dist/' },
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
+    compress: true,
+  },
+};
+
+if (isDev && config.plugins) {
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(new ReactRefreshWebpackPlugin());
+  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: false }));
+}
+
+if (!isDev && config.plugins) {
+  config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+}
+
+export default config;
