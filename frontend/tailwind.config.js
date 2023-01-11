c2551b154f72e1fb5e139669fdfdbc035ba1a091
howdyfrom2019 <tjd1emd@naver.com>
2023-01-11 13:40:28 +0900

    feat: basic font extension setting.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,33 @@
+/** @type {import('tailwindcss').Config }*/
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: '#fcc563',
+        black: '#333333',
+        white: '#ffffff',
+        whiteAlpha: 'rgba(255, 255, 255, 0.4)',
+        primary: '#32315b',
+        primaryDark: '#313059',
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto'],
+      },
+      scale: {
+        'reverse': '-1',
+      },
+      boxShadow: {
+        'search-bar': '-2px -1px 0 1px #383762',
+        'section': '-2px -1px 2px #383762',
+      },
+    },
   },
   plugins: [],
 }
