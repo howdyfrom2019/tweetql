8bd4ee2c3f52b0e3f22f81c9f94f2c816f9b7676
howdyfrom2019 <tjd1emd@naver.com>
2023-01-16 13:39:49 +0900

    feat: get all championinfo

---
+++ backend/api/cdn/getAllChampionInfo.js
@@ -0,0 +1,22 @@
+import fetch from 'node-fetch';
+
+export default async function getAllChampionInfo(version) {
+  const rowData = await fetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion.json`, {
+    method: "GET",
+    headers: {
+      "Content-Type": 'application/json'
+    },
+    credentials: 'same-origin',
+    mode: 'cors'
+  });
+
+  const { data } = await rowData.json();
+  const dataWithChampionId = [];
+  for (const champion in data) {
+    const { key, ...props } = data[champion];
+    const index = Number(key);
+    dataWithChampionId[index] = { key, ...props };
+  }
+  // console.log(dataWithChampionId);
+  return dataWithChampionId;
+}
