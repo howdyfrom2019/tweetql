40596935429169615c298d4b7f39fc1f559fe1c1
howdyfrom2019 <tjd1emd@naver.com>
2023-03-20 13:21:16 +0900

    feat: next-redux-wrapper and hydrate setting

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,22 +1,19 @@
 import type { AppProps } from 'next/app';
 import { useApollo } from '../../client';
 import { ApolloProvider } from '@apollo/client';
+import wrapper from '@/store/store';
 import 'tailwindcss/tailwind.css';
 import '../../index.css';
-import { Provider } from 'react-redux';
-import store from '@/store/store';
 
 function _app({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
   return (
     <>
       <ApolloProvider client={apolloClient}>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
+        <Component {...pageProps} />
       </ApolloProvider>
     </>
   );
 }
 
-export default _app;
+export default wrapper.withRedux(_app);
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers/RootReducer';
+import { createWrapper } from 'next-redux-wrapper';
+import { Context } from 'next-redux-wrapper/lib';
 
 const store = configureStore({
   reducer: rootReducer
+});
+
+const makeStore = (context: Context) => store;
+
+const wrapper = createWrapper(makeStore, {
+  debug: process.env.NODE_ENV !== 'production',
 })
 
-export default store;
+export default wrapper;
